a58b0b1bbcbfbf84a9a031e22f4158c267f2e848
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-05-24 02:53:21 +0300

    added footer

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+	return <section className={classes.footer}></section>;
+};
+
+export default Footer;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import Navigation from "./Nav/Navigation";
 import classes from "./Layout.module.css";
+import Footer from "./Footer/Footer";
 
 const Layout = (props) => {
 	return (
 		<Fragment>
 			<Navigation />
 			<main className={classes.container}>{props.children}</main>
+			<Footer />
 		</Fragment>
 	);
 };
